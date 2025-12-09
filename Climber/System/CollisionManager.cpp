d609ac38040b1fe8ea65a35aa2d8435e618e57ae
kudousyota <fko2547040@stu.o-hara.ac.jp>
2025-12-09 23:46:33 +0900

    当たり判定実装途中

--- Climber/System/CollisionManager.cpp
+++ Climber/System/CollisionManager.cpp
@@ -1,7 +1,19 @@
 #include "CollisionManager.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "Stage.h"
+#include "GameObject.h"
 
-void CollisionManager::CheckCollisions(std::shared_ptr<Player> m_pPlayer, std::shared_ptr<Enemy> m_pEnemy)
+void CollisionManager::CheckCollisions(std::shared_ptr<Player> m_pPlayer, std::shared_ptr<Enemy> m_pEnemy, std::shared_ptr<Stage> m_pStage)
 {
 	// プレイヤーと敵の当たり判定をチェック
-	
+	if (m_pPlayer->IsHit(*m_pEnemy))
+	{
+		// 当たっていたら押し出し処理を行う
+		Vec2 push = m_pPlayer->FixPos(*m_pEnemy);
+		// プレイヤーの位置を更新
+		m_pPlayer->GetRect().SetX(m_pPlayer->GetRect().GetX() + push.x);
+		m_pPlayer->GetRect().SetY(m_pPlayer->GetRect().GetY() + push.y);
+	}
+	// プレイヤーとステージの当たり判定をチェック
 }
--- Climber/System/CollisionManager.h
+++ Climber/System/CollisionManager.h
@@ -3,13 +3,15 @@
 #include <memory>
 class Player;
 class Enemy;
+class Stage;
 class CollisionManager
 {
 public:
 	//全ての当たり判定をチェックする
 	static void CheckCollisions(
 		std::shared_ptr<Player>m_pPlayer,
-		std::shared_ptr<Enemy>m_pEnemy
+		std::shared_ptr<Enemy>m_pEnemy,
+		std::shared_ptr<Stage>m_pStage
 	);
 
 };
